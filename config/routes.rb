cb52b18a0a85d71337b99805c16b16ea1c63102e
John Maliszewski <johnmal96@yahoo.com>
2017-11-18 13:34:57 +0000

    fully functional

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,25 @@
 Rails.application.routes.draw do
+  get 'admin' => 'admin#index'
+  get 'admin/index'
+  get  'static_pages/about'
+  get  'static_pages/help'
+  get  'static_pages/home'
+
+  controller :sessions do
+    get  'login' => :new
+    post 'login' => :create
+    delete 'logout' => :destroy
+  end
   
-  
+  resources :users
   resources :orders
   resources :line_items
   resources :carts
-  get 'store/index'
+  root 'store#index', as: 'store_index'
+  
+  resources :products 
 
-  resources :products
-  get 'static_pages/home'
 
-  get 'static_pages/help'
-
-  get 'static_pages/about'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  root 'store#index'
-
+# For details on the DSL available within this file, see
+# http://guides.rubyonrails.org/routing.html
 end
